539c92729e47740fdd6ac35e02b508ada57372ea
ebarrera2019263 <ebarrera2019263>
2024-05-06 17:08:31 -0600

    se creo el proyecto, se agrego archivo de rutas, archivos de css y se configuro la parte del cliente del backend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// App.jsx
+import AppRoutes from './Routes'; // Asegúrate de tener la ruta correcta
+
+function App() {
+  return (
+    <div>
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
